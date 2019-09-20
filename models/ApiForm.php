440f78a56581968d905afc63048b4a79cd889520
Mark Raymond Reformado <mark.raymond@dpodium.com>
2019-09-20 12:39:26 +0800

    - remove commented part - apply formatting

--- models/ApiForm.php
+++ models/ApiForm.php
@@ -10,13 +10,13 @@ use \common\models\MerchantServer;
  * @property string $action
  * @property string $api_key
  */
-abstract class ApiForm extends \yii\base\Model {
+abstract class ApiForm extends \yii\base\Model
+{
 
     public $action;
     public $timestamp;
     public $api_key;
     public $version;
-
     public $merchant_id;
     public $api_secret;
 
@@ -25,11 +25,11 @@ abstract class ApiForm extends \yii\base\Model {
      */
     public function rules() {
         return [
-            [['api_key', 'timestamp', 'action'], 'required'],
-            [['timestamp'], 'integer'],
-            [['api_key'], 'string', 'max' => 64],
-            [['version'], 'string', 'max' => 255],
-            [['api_key'], 'validateApiKey'],
+            [ [ 'api_key', 'timestamp', 'action' ], 'required' ],
+            [ [ 'timestamp' ], 'integer' ],
+            [ [ 'api_key' ], 'string', 'max' => 64 ],
+            [ [ 'version' ], 'string', 'max' => 255 ],
+            [ [ 'api_key' ], 'validateApiKey' ],
         ];
     }
 
@@ -38,49 +38,36 @@ abstract class ApiForm extends \yii\base\Model {
      */
     public function attributeLabels() {
         return parent::attributeLabels() + [
-            'api_key' => 'api_key',
+            'api_key'   => 'api_key',
             'timestamp' => 'timestamp',
             'signature' => 'signature',
-            'version' => 'version',
+            'version'   => 'version',
         ];
     }
 
     public function validateSignature($attribute, $params) {
         $sig = $this->generateSignature();
-        if ($sig !== $this->$attribute) {
+        if ( $sig !== $this->$attribute ) {
             $this->addError($attribute, 'Invalid signature');
         }
     }
 
     public function generateSignature($array = []) {
         return static::genSignature(array_merge($array, [
-            'action' => $this->action,
-            'timestamp' => $this->timestamp,
-            'api_key' => $this->api_key,
-            'api_secret' => $this->api_secret,
+                    'action'     => $this->action,
+                    'timestamp'  => $this->timestamp,
+                    'api_key'    => $this->api_key,
+                    'api_secret' => $this->api_secret,
         ]));
     }
 
     public static function genSignature($array = []) {
         ksort($array);
         $s = "";
-        foreach($array as $key => $value) {
+        foreach ( $array as $key => $value ) {
             $s .= $key . ':' . $value;
         }
         return sha1($s);
     }
 
-
-//
-//    public function validateApiKey($attribute, $params) {
-//        $merchant_server = MerchantServer::getBasicInfoFromKey($this->$attribute);
-//        if (!isset($merchant_server)) {
-//            $this->addError($attribute, $attribute . ' is invalid.');
-//        } else {
-//            $this->api_secret = $merchant_server['api_secret'];
-//            $this->merchant_id = $merchant_server['merchant_id'];
-//        }
-//    }
-
-
-        }
+}
--- models/api/InitiateVerificationSessionForm.php
+++ models/api/InitiateVerificationSessionForm.php
@@ -5,21 +5,12 @@ namespace dpodium\pipwave\sdk\models\api;
 /**
  * InitiateVerificationSessionForm is the model behind the initiate-verification-session API.
  */
-class InitiateVerificationSessionForm extends \dpodium\pipwave\sdk\models\ApiForm {
+class InitiateVerificationSessionForm extends \dpodium\pipwave\sdk\models\ApiForm
+{
 
     const ACTION_CODE = 'initiate-verification-session',
-    MODE_KEYWORD_LIVEFEED = 'livefeed',
-    MODE_KEYWORD_UPLOAD = 'upload';
-//    const MODULES = [
-//        self::TYPE_IDENTIFICATION => [
-//            'class' => '\common\components\document_verification_module\IdentificationVerification',
-//        ],
-//        self::TYPE_CREDIT_CARD => [
-//            'class' => '\common\components\document_verification_module\CreditCardVerification',
-//        ],
-//    ];
-//
-
+            MODE_KEYWORD_LIVEFEED = 'livefeed',
+            MODE_KEYWORD_UPLOAD = 'upload';
 
     public $types_of_verification; //JSON
     public $user_id;
@@ -35,12 +26,12 @@ class InitiateVerificationSessionForm extends \dpodium\pipwave\sdk\models\ApiFor
      */
     public function rules() {
         return array_merge(parent::rules(), [
-                [['user_id', 'signature'], 'required'],
-                [['extra_param1', 'extra_param2', 'extra_param3', 'types_of_verification', 'notification_url', 'allowed_mode'], 'string', 'max' => 255],
-                [['user_id'], 'string', 'max' => 32],
-                [['signature'], 'validateSignature'],
-                [['types_of_verification'], 'validateTypesOfVerification'],
-                [['allowed_mode'], 'validateAllowedSource'],
+            [ [ 'user_id', 'signature' ], 'required' ],
+            [ [ 'extra_param1', 'extra_param2', 'extra_param3', 'types_of_verification', 'notification_url', 'allowed_mode' ], 'string', 'max' => 255 ],
+            [ [ 'user_id' ], 'string', 'max' => 32 ],
+            [ [ 'signature' ], 'validateSignature' ],
+            [ [ 'types_of_verification' ], 'validateTypesOfVerification' ],
+            [ [ 'allowed_mode' ], 'validateAllowedSource' ],
         ]);
     }
 
@@ -50,35 +41,33 @@ class InitiateVerificationSessionForm extends \dpodium\pipwave\sdk\models\ApiFor
     public function attributeLabels() {
         return array_merge(parent::attributeLabels(), [
             'types_of_verification' => 'types_of_verification',
-            'user_id' => 'user_id',
-            'extra_param1' => 'extra_param1',
-            'extra_param2' => 'extra_param2',
-            'extra_param3' => 'extra_param3',
-            'notification_url' => 'notification_url',
-            'allowed_mode' => 'allowed_mode',
-            'signature' => 'signature',
+            'user_id'               => 'user_id',
+            'extra_param1'          => 'extra_param1',
+            'extra_param2'          => 'extra_param2',
+            'extra_param3'          => 'extra_param3',
+            'notification_url'      => 'notification_url',
+            'allowed_mode'          => 'allowed_mode',
+            'signature'             => 'signature',
         ]);
     }
 
     public function generateSignature($array = []) {
-        if (!empty($this->user_id)) {
+        if ( !empty($this->user_id) ) {
             $array['user_id'] = $this->user_id;
         }
-        if (!empty($this->extra_param1)) {
+        if ( !empty($this->extra_param1) ) {
             $array['extra_param1'] = $this->extra_param1;
         }
-        if (!empty($this->extra_param2)) {
+        if ( !empty($this->extra_param2) ) {
             $array['extra_param2'] = $this->extra_param2;
         }
-        if (!empty($this->extra_param3)) {
+        if ( !empty($this->extra_param3) ) {
             $array['extra_param3'] = $this->extra_param3;
         }
-        if (!empty($this->notification_url)) {
+        if ( !empty($this->notification_url) ) {
             $array['notification_url'] = $this->notification_url;
         }
         return parent::generateSignature($array);
     }
-
-
 
 }
